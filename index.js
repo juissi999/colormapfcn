80d9be9541ff4112d95971913807644ecfc8e0a1
juissi999 <jussi.kaatiala@gmail.com>
2021-06-20 12:44:51 +0300

    Add raw init of cmapFcn object

---
+++ index.js
@@ -0,0 +1,10 @@
+const cmapFcn = {
+  init: function (rstart, gstart, bstart) {
+    this.rstart = rstart
+    this.gstart = gstart
+    this.bstart = bstart
+  },
+  rstart: 0,
+  gstart: 0,
+  bstart: 0
+}
