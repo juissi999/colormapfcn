e0aac7ce7d157618ae4aa6ed5e04cc83ce5bf802
juissi999 <jussi.kaatiala@gmail.com>
2021-06-20 12:51:48 +0300

    Add calculation logic, display values in page

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const cmapFcn = {
-  init: function (rstart, gstart, bstart) {
+  init: function (rstart, gstart, bstart, rincrement, gincrement, bincrement) {
     this.rstart = rstart
     this.gstart = gstart
     this.bstart = bstart
+    this.rincrement = rincrement
+    this.gincrement = gincrement
+    this.bincrement = bincrement
+  },
+  getValue: function (index) {
+    return {
+      r: (this.rstart + this.rincrement * index) % 256,
+      g: (this.gstart + this.gincrement * index) % 256,
+      b: (this.bstart + this.bincrement * index) % 256
+    }
   },
   rstart: 0,
   gstart: 0,
-  bstart: 0
+  bstart: 0,
+  rincrement: 1,
+  gincrement: 1,
+  bincrement: 1
 }
